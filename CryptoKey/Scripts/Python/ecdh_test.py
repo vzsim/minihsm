fcd57fe9c019d656553b156a9f1e295741353085
Telman Islamov <t.islamov@aladdin.ru>
2025-08-29 11:32:38 +0300

    minor refinements

--- CryptoKey/Scripts/Python/ecdh_test.py
+++ CryptoKey/Scripts/Python/ecdh_test.py
@@ -56,12 +56,17 @@ class DiffHell:
 	
 	def init_cipher(self, sk):
 		self.secret_key = sk
-		self.cipher = Cipher(algorithms.AES(sk), modes.CBC(iv))
+		self.cipher = Cipher(algorithms.AES(sk), modes.CBC(self.iv))
 	
 	def encrypt_msg(self, plain_bytes):
 		encryptor = self.cipher.encryptor()
 		cipher_text = encryptor.update(plain_bytes) + encryptor.finalize()
 		return cipher_text
+
+	def decrypt_msg(self, cipher_bytes):
+		decryptor = self.cipher.decryptor()
+		plain_text = decryptor.update(cipher_bytes) + decryptor.finalize()
+		return plain_text
 
 
 def main_func():
@@ -75,18 +80,18 @@ def main_func():
 	trn(           pcsc.asciiToHex('00A40400') + ln('A00000000101'),          expsw = 0x9000, descr = 'Select CryptoKey')
 	response = trn(pcsc.asciiToHex('00220000') + ln(uncompress(dh.publ_key)), expsw = 0x9000, descr = 'Generate shared secret')
 
-	card_publ_raw = response[1:65]
-	card_shared_raw = response[65:-2]
-
-	card_publ_key = ec.EllipticCurvePublicNumbers(int.from_bytes(card_publ_raw[0:32], 'big'), int.from_bytes(card_publ_raw[32:64], 'big'), ec.SECP256K1())
+	card_pubk_x = int.from_bytes(response[1:33], 'big')
+	card_pubk_y = int.from_bytes(response[33:65], 'big')
+	
+	card_publ_key = ec.EllipticCurvePublicNumbers(card_pubk_x, card_pubk_y, ec.SECP256K1())
 	host_shared = dh.gen_shared(card_publ_key.public_key())
 
 	print("\nHost public key : ", uncompress(dh.publ_key))
-	print("Card  public key: ", hexToAscii(card_publ_raw))
+	print("Card  public key: ", hexToAscii(response[0:65]))
 	print("Host  shared key: ", hexToAscii(host_shared))
-	print("Card  shared key: ", hexToAscii(card_shared_raw))
+	print("Card  shared key: ", hexToAscii(response[65:-2]))
 
-	print("Are values equal? ", hexToAscii(host_shared) == hexToAscii(card_shared_raw))
+	print("Are values equal? ", hexToAscii(host_shared) == hexToAscii(response[65:-2]))
 	pcsc.disconnect(card)
 
 main_func()
